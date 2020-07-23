aaef4205206125d7d34f9c0129e8d53070497d81
Paul Reioux <reioux@gmail.com>
2020-07-23 10:32:09 -0700

    compat: dst_cache.c: remove rt6_get_cookie function
    This function's already implemented in the hybrid Linux 4.1.x kernel
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- src/compat/dst_cache/dst_cache.c
+++ src/compat/dst_cache/dst_cache.c
@@ -16,13 +16,13 @@
 #if IS_ENABLED(CONFIG_IPV6)
 #include <net/ip6_fib.h>
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(4, 2, 0) && LINUX_VERSION_CODE >= KERNEL_VERSION(3, 17, 0)) || LINUX_VERSION_CODE < KERNEL_VERSION(3, 16, 50)
-static inline u32 rt6_get_cookie(const struct rt6_info *rt)
-{
-	if ((unlikely(rt->dst.flags & DST_NOCACHE) && rt->dst.from))
-		rt = (struct rt6_info *)(rt->dst.from);
-
-	return rt->rt6i_node ? rt->rt6i_node->fn_sernum : 0;
-}
+//static inline u32 rt6_get_cookie(const struct rt6_info *rt)
+//{
+//	if ((unlikely(rt->dst.flags & DST_NOCACHE) && rt->dst.from))
+//		rt = (struct rt6_info *)(rt->dst.from);
+//
+//	return rt->rt6i_node ? rt->rt6i_node->fn_sernum : 0;
+//}
 #endif
 #endif
 #include <uapi/linux/in.h>
